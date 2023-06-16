85fb0cecb1f470ae5d45a704947bc733930d4104
Aiden <kim_aiden@naver.com>
2023-06-16 14:04:46 +0900

    빈 생명주기 콜백

---
+++ Spring-Boot-Basics/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,36 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+    ConfigurableApplicationContext ac
+            = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close(); // 스프링 컨테이너 종료 -> ConfigurableApplicationContext 인터페이스를 사용하거나
+                    //                      AnnotationConfigApplicationContext를 사용해야함
+    }
+
+
+
+    @Configuration
+    static class LifeCycleConfig{
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+
+
+    }
+}
--- Spring-Boot-Basics/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ Spring-Boot-Basics/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,78 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
-    static class
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+//        connect();
+//        call("초기화 연결 메시지");
+    }
+
+    // url 지정
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 연결시 호출
+    public void connect() {
+        System.out.println("connect = " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call = " + url + ", message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("disconnect = " + url);
+    }
+
+    @PostConstruct
+    public void init(){
+        connect();
+        call("초기화 연결 메시지");
+        System.out.println("NetworkClient.init");
+    }
+
+    @PreDestroy
+    public void close() {
+        disconnect();
+    }
+
+
+// -------------- 빈 등록 초기화, 소멸메서드 지정---------------------
+//    public void init(){
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//
+//    public void close() {
+//        disconnect();
+//    }
+
+
+// -------------------인터페이스 활용----------------------------
+//    @Override
+//    public void destroy() throws Exception {
+//        disconnect();
+//    }
+//
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
 }
+
+
 
 
 
